c5d43c33ed69f62ddb2075e30a771323dc40e25b
IlyaM9777 <milya9397@gmail.com>
2026-01-02 12:17:37 +0300

    7

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,7 +4,11 @@ import styles from "./Logo.module.css";
 function Logo() {
   return (
     <Link to="/">
-      <img src="/logo.png" alt="WorldWise logo" className={styles.logo} />
+      <img
+        src="/logo.png"
+        alt="World Travel Diary logo"
+        className={styles.logo}
+      />
     </Link>
   );
 }
